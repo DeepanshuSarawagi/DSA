9d0112a5aa043e5c1821cecf77a327f27edfe395
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-23 21:26:47 +0530

    Understand the theory behind Array

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -2,6 +2,13 @@ package com.company;
 
 public class Main {
 
+    /*
+    Arrays are stored as one contiguous block in memory.
+    By that we mean, lets say if we have an array of 100 items and the memory address of first item is at position
+    100, and the size is 200 bytes then the entire length would be for 200 bytes starting from memory address 100.
+    Therefore, we can say that the array items are not scattered in memory.
+     */
+
     public static void main(String[] args) {
         int[] intArray = new int[7];
 
