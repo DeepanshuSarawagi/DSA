2fa63eb2ec778fdd4f9d58a88f8f46a3ec195e93
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-23 21:35:18 +0530

    Understand the theory behind Array

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -7,6 +7,9 @@ public class Main {
     By that we mean, lets say if we have an array of 100 items and the memory address of first item is at position
     100, and the size is 200 bytes then the entire length would be for 200 bytes starting from memory address 100.
     Therefore, we can say that the array items are not scattered in memory.
+
+    Every element occupies the same amount of space in memory. For example, in Java, an int is 4 bytes long. Hence,
+    every element of int array will occupy 4 bytes of space in memory.
      */
 
     public static void main(String[] args) {
