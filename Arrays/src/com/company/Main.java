c976b67fa6713664f90cae9a268f04f028f62e21
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-23 21:18:59 +0530

    Learn to create a basic Array

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] intArray = new int[7];
+
+        for (int i=0; i<7; i++) {
+            intArray[i] = i + 10;
+        }
+
+        // Printing the elements of array
+        for (int i=0; i<intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
     }
 }
