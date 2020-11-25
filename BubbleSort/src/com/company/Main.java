91f600d16af2dacc5b0bad59f1194a69a723d629
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-25 19:16:42 +0530

    Theory of Bubble Sort algorithm

--- BubbleSort/src/com/company/Main.java
+++ BubbleSort/src/com/company/Main.java
@@ -1,6 +1,12 @@
 package com.company;
 
 public class Main {
+    /*
+    Bubble Sort algorithm is a stable algorithm. It has a quadratic time complexity =, hence, one of the least efficient
+    sorting algorithms in place.
+    The Big(O) notation is n^2, hence, makes the time complexity as quadratic. For sorting 100 items, we would need to
+    loop 10000 times to sort an array.
+     */
 
     public static void main(String[] args) {
 //        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
