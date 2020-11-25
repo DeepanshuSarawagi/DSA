47d26fcd1303bfccda42019d6130382ff6c233d8
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-25 17:05:36 +0530

    Understand the theory behind Array

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -10,6 +10,9 @@ public class Main {
 
     Every element occupies the same amount of space in memory. For example, in Java, an int is 4 bytes long. Hence,
     every element of int array will occupy 4 bytes of space in memory.
+
+    if the memory starts at memory address x and there are y elements in array and if we know the index of the starting
+    element, then we can find the memory address of the ith element using the following expression (x + i*y)
      */
 
     public static void main(String[] args) {
