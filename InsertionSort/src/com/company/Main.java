6709997b27fff17a3df27ed02572e941069b59cd
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-26 21:42:21 +0530

    Understand the basic theory Insertion Sort algorithm

--- InsertionSort/src/com/company/Main.java
+++ InsertionSort/src/com/company/Main.java
@@ -2,7 +2,16 @@ package com.company;
 
 public class Main {
 
+    /*
+    Insertion Sort algorithm is also an in-place algorithm like Bubble Sort and Selection Sort
+    It has a time complexity of quadratic. Which means for every 100 elements, it would take 10,000 steps to sort them
+    The Big O notation value is O(n^2).
+    It is a stable algorithm, which means we the position of two identical elements in an array will not be changed
+    while performing sort operation.
+     */
+
     public static void main(String[] args) {
-	// write your code here
+	    int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
     }
 }
