0ad2b1ebf1497a9e694f446ad9234ba9761b3694
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-11-26 21:46:17 +0530

    Understand the theory behind Insertion Sort algorithm

--- InsertionSort/src/com/company/Main.java
+++ InsertionSort/src/com/company/Main.java
@@ -8,6 +8,13 @@ public class Main {
     The Big O notation value is O(n^2).
     It is a stable algorithm, which means we the position of two identical elements in an array will not be changed
     while performing sort operation.
+
+    How does Insertion Sort algorithm work?
+    In this case, we will assume that the first index position of array is already sorted. Hence the firstUnsortedIndex
+    will be set to 1. We will then compare the element at position 1 with the element in position 0. If the element at
+    position 1 is greater than element at position 0, the firstUnsortedIndex will be updated to position 2. If not, the
+    element at position 0 will be shifted right (to position 1) and the unsorted element will be inserted at position 0.
+
      */
 
     public static void main(String[] args) {
