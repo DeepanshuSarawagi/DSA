167575a8f172553259a0e7045a607fc09dc049d9
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-14 20:39:53 +0530

    Understand the theory behind shell sort algorithm

--- ShellSort/src/com/company/Main.java
+++ ShellSort/src/com/company/Main.java
@@ -2,8 +2,16 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	    int[] intArray = {-20, 89, 96, -71, 105, 692, -897};
+    /**
+     * Insertion sort algorithm chooses which element to insert using a gap of 1
+     * Shell sort starts out using a larger gap value
+     * As the algorithm runs, the gap reduces
+     * In this implementation we will use the gap value of array.length/2
+     *
+     */
 
+    public static void main(String[] args) {
+
+	    int[] intArray = {-20, 89, 96, -71, 105, 692, -897};
     }
 }
