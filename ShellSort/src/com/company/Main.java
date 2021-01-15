0433dfa8d5fda1eb73bb05a2685cbdaf624bab8d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-15 21:36:24 +0530

    Learn the shell sort algorithm implementation

--- ShellSort/src/com/company/Main.java
+++ ShellSort/src/com/company/Main.java
@@ -7,11 +7,36 @@ public class Main {
      * Shell sort starts out using a larger gap value
      * As the algorithm runs, the gap reduces
      * In this implementation we will use the gap value of array.length/2
-     *
+     * Shell sort is an unstable algorithm unlike insertion sort
      */
 
     public static void main(String[] args) {
 
 	    int[] intArray = {-20, 89, 96, -71, 105, 692, -897};
+
+	    for (int gap = intArray.length/2; gap > 0; gap /= 2) {
+
+	        for (int i = gap; i < intArray.length; i++) {
+	            int newElement = intArray[i];
+
+	            int j = i;
+	            while (j >= gap && intArray[j - gap] > newElement) {
+	                intArray[j] = intArray[j-gap];
+	                j-=gap;
+                }
+	            intArray[j] = newElement;
+
+                System.out.println("Array after sort is now: ");
+                for (int k : intArray) {
+                    System.out.print(k + " ");
+                }
+                System.out.println();
+            }
+        }
+
+
+	    for (int i : intArray) {
+            System.out.print(i + " ");
+        }
     }
 }
